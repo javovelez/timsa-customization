f1a8fa28f760560a3864bd1ccca6e978f04f6a73
javovelez <javovelez@gmail.com>
2020-09-24 09:47:31 -0300

    update

--- timsa_account/__manifest__.py
+++ timsa_account/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "timsa_init",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Parametrización de TIMSA""",
 
     'description': """
-        Long description of module's purpose
+        
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Javo",
+    'website': "www.timsasa.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,16 +19,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','l10n_ar'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'data/timsa_data.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        
     ],
 }
---
+++ timsa_homologation/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "timsa_init",
+
+    'summary': """
+       Parametrización de TIMSA""",
+
+    'description': """
+        
+    """,
+
+    'author': "Javo",
+    'website': "www.timsasa.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','l10n_ar'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/timsa_data.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        
+    ],
+}
