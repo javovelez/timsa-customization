d433794e406b14d1943b518fabe040bcd71d0306
Javier Velez <javovelez@gmail.com>
2021-01-14 09:13:29 -0300

    planteo cambios a fututo

---
+++ timsa-sale/models/res_partner.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+class partnerSubcontratista(models.Model):
+
+    #Aqui debo sumar un campo bool que indique que si es o no subcontratista
+    #En base a ese campo puedo generar en la vista como tag
+    #Tengo que pensarlo mejor ya que no creo que lo haga de esta manera, aunque el 
+    #campo booleano lo voy a necesitar de todos modos
