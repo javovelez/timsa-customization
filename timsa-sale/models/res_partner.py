1e57f547a3aca0ad86d8d75be276494256627609
Javier Velez <javovelez@gmail.com>
2021-01-16 22:09:27 -0300

    mejoro vistas y avanzo con lógica

--- timsa-sale/models/res_partner.py
+++ timsa-sale/models/res_partner.py
@@ -2,6 +2,14 @@ from odoo import fields, models
 
 class partnerSubcontratista(models.Model):
 
+    
+    subcontractor = fields.Boolean(
+        string='Es subcontratista',
+    )
+
+    
+    
+    
     #Aqui debo sumar un campo bool que indique que si es o no subcontratista
     #En base a ese campo puedo generar en la vista como tag
     #Tengo que pensarlo mejor ya que no creo que lo haga de esta manera, aunque el 
--- timsa-sale/models/sale_order.py
+++ timsa-sale/models/sale_order.py
@@ -5,8 +5,16 @@ class saleOrdertimsa(models.Model):
     _inherit = "sale.order"
     certificate_name = fields.Char(string='Nombre', required=True)
     oc_code = fields.Char(string='OC')
-    bpm_code = fields.Char(string='Código BPM', required=False)
+    code = fields.Char(string='Código', required=False)
     ruta = fields.Text(string='Ruta a certificado')
+    
+
+    
+    # field_name_ids = fields.Many2many(
+    #     string='Subcontratistas',
+    #     comodel_name='', buscar el nombre exacto purchase
+    # )
+    
 
     #Pendientes:
     #task_id: tareas que se certifican
